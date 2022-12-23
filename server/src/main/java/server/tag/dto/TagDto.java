9bbd7236706aab83c9f8519aa9eb13c2fce65924
Ingsu <ingsu814@gmail.com>
2022-12-23 17:53:13 +0900

    feat: Tag Controller, Dto, Entity, Mapper 기본 설정 완료(Service 및 기타 추가적인 부분 구현 필요)

--- server/src/main/java/server/tag/dto/TagDto.java
+++ server/src/main/java/server/tag/dto/TagDto.java
@@ -1,10 +1,16 @@
 package server.tag.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
+import javax.persistence.*;
+
 
 public class TagDto {
-
+    public static class Response {
+        String tagName;
+        String tagExplanation;
+    }
 }
-
