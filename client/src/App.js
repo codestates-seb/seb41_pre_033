11881f104e2489be31a17981e4b77355c3bb892c
EliseCho13 <kellycho1031@gmail.com>
2022-12-23 23:30:55 +0900

    feat: 임시도메인으로 get 요청 테스트 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './header/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Ask from "./questions/pages/ask/Ask";
@@ -6,9 +6,25 @@ import Navbar from "./components/navbar/Navbar";
 import Questions from "./questions/Questions";
 import Tags from "./tags/Tags";
 import Users from "./users/Users";
+import User from "./users/pages/User";
 
 function App() {
+  const tempDomain = "http://localhost:3001";
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUser();
+  }, [])
+  const getUser = () => {
+    return fetch(tempDomain+"/users")
+    .then((res) => res.json())
+    .then((data) => {
+      setUsers(data)
+    })
+  };
+
   return (
+    <div>
     <Router>
       <Header />
       <Navbar />
@@ -16,12 +32,14 @@ function App() {
         <Route path="/" element={<Questions />} />
         <Route path="/questions" element={<Questions />} />
         <Route path="/tags" element={<Tags />} />
-        <Route path="/users" element={<Users />} />
+        <Route path="/users" element={<Users users={users} />} />
         <Route path="/dev1" /> {/* 코드 리뷰용 임시 루트1 */}
         <Route path="/dev2" /> {/* 코드 리뷰용 임시 루트2 */}
         <Route path="/dev3" /> {/* 코드 리뷰용 임시 루트3 */}
+        <Route path="/user" element={<User />}/>
       </Routes>
     </Router>
+    </div>
   );
 }
 
--- client/src/users/Users.js
+++ client/src/users/Users.js
@@ -1,34 +1,18 @@
-import './users.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen, faLocationDot } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+//다수의 유저 보여주는 페이지
+import './users.css'
+import React from 'react';
+import { UserItem } from "./UserItem";
 
-function Users () {
+const Users = ({users}) => {
     return (
-    <div id="user-body">
-        <div id="user-header">
-            <div id="user-pfp">A</div>
-            <div id="user-text">
-                <div id="user-name">A Name</div>
-                <div id="user-info">
-                    <div className='user-info-item'><FontAwesomeIcon className="user-icon" icon={faGithub} /></div>
-                    <div className='user-info-item'><FontAwesomeIcon className="user-icon" icon={faLocationDot} /> From 맛동산</div>
-                </div>
-                <div className='user-info-item'>Title here</div>
-            </div>
-            <div id="edit-button-container">
-                <button id="edit-profile"><FontAwesomeIcon className="user-icon" icon={faPen} />Edit profile</button>
-            </div>
+        <div className='user-list-wrapper'>
+            <ul className='user-list-container'>
+                {users.map((e) => {
+                    return <UserItem key={e.id} user={e}/>
+                })}
+            </ul>
         </div>
-        <div id="user-menu">
-            <div className='user-menu-item'>Profile</div>
-            <div className='user-menu-item'>Activity</div>
-            <div className='user-menu-item'>Saves</div>
-            <div className='user-menu-item'>Settings</div>
-        </div>
-    </div>
     )
-
 }
 
 export default Users;
