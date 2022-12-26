d4c5d9d5d112c35792178256721e782444ae496a
Ingsu <ingsu814@gmail.com>
2022-12-26 21:17:10 +0900

    fix: Answer 관련 작성 완료, 연관된 Question 엔티티 항목들 수정 완료

--- server/src/main/java/server/answer/controller/AnswerController.java
+++ server/src/main/java/server/answer/controller/AnswerController.java
@@ -1,10 +1,16 @@
 package server.answer.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.answer.dto.AnswerDto;
+import server.answer.entity.Answer;
 import server.answer.mapper.AnswerMapper;
 import server.answer.service.AnswerService;
+import server.dto.SingleResponseDto;
+import server.question.entity.Question;
+import server.question.repository.QuestionRepository;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
@@ -15,31 +21,63 @@ public class AnswerController {
     private static final int SIZE = 36;
     private AnswerService answerService;
     private AnswerMapper answerMapper;
+    private final QuestionRepository questionRepository;
 
-    public AnswerController(AnswerService answerService, AnswerMapper answerMapper) {
+    public AnswerController(AnswerService answerService, AnswerMapper answerMapper,
+                            QuestionRepository questionRepository) {
         this.answerService = answerService;
         this.answerMapper = answerMapper;
+        this.questionRepository = questionRepository;
     }
 
-    @PostMapping
-    public ResponseEntity postAnswer(@Valid @RequestBody AnswerDto.Post requestBody){
-        return null;
+    @PostMapping("/{question-id}")
+    public ResponseEntity postAnswer(@PathVariable("question-id") @Positive long questionId,
+                                     @Valid @RequestBody AnswerDto.Post requestBody){
+        Question targetQuestion = questionRepository.findByQuestionId(questionId);
+        Answer answer = answerService.createAnswer(targetQuestion, answerMapper.answerPostToAnswer(requestBody));
+
+        return new ResponseEntity(
+                new SingleResponseDto<>(answerMapper.answerToAnswerResponseDto(answer)),
+                        HttpStatus.CREATED);
     }
-    @PatchMapping("/{answer-id}")
+    @PatchMapping("/edit/{answer-id}")
     public ResponseEntity patchAnswer(@PathVariable("answer-id") @Positive long answerId,
-                                      @Valid @RequestBody AnswerDto.Patch requestBody){
-        return null;
+                                      @Valid @RequestBody AnswerDto.PatchAnswer patchAnswerDto){
+        patchAnswerDto.setAnswerId(answerId);
+
+        Answer answer =
+                answerService.updateAnswer(answerMapper.patchAnswerDtoToAnswer(patchAnswerDto));
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(answerMapper.answerToAnswerResponseDto(answer)),
+                HttpStatus.OK);
     }
-    @GetMapping("/{answer-id}")
-    public ResponseEntity getAnswer(@PathVariable("answer-id") @Positive long answerId){
-        return null;
+
+    @PatchMapping("/edit/{answer-id}/vote")
+    public ResponseEntity patchVote(@PathVariable("answer-id") @Positive long answerId,
+                                    @RequestParam String updown,
+                                    @RequestBody AnswerDto.PatchVote patchVoteDto){
+        patchVoteDto.setAnswerId(answerId);
+        Answer answer =
+                answerService.updateVote(patchVoteDto.getAnswerId(),patchVoteDto.getUserId(),updown);
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(answerMapper.answerToAnswerResponseDto(answer)),
+                HttpStatus.OK);
     }
-    @GetMapping
-    public ResponseEntity getAnswers(@Positive @RequestParam int page){
-        return null;
+
+    @PatchMapping("/edit/{answer-id}/accept")
+    public ResponseEntity patchAccept(@PathVariable("answer-id") @Positive long answerId,
+                                     @RequestBody AnswerDto.PatchAccept patchAcceptDto){
+        patchAcceptDto.setAnswerId(answerId);
+        Answer answer =
+                answerService.updateAccept(patchAcceptDto.getAnswerId(),patchAcceptDto.getUserId());
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(answerMapper.answerToAnswerResponseDto(answer)),
+                HttpStatus.OK);
     }
+
     @DeleteMapping("/{answer-id}")
     public ResponseEntity deleteAnswer(@PathVariable("answer-id") @Positive long answerId){
-        return null;
+        answerService.deleteAnswer(answerId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- server/src/main/java/server/answer/dto/AnswerDto.java
+++ server/src/main/java/server/answer/dto/AnswerDto.java
@@ -1,19 +1,50 @@
 package server.answer.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public class AnswerDto {
     @Getter
     public static class Post{
+        private Long userId;
         private String body;
     }
 
     @Getter
-    public static class Patch{
+    public static class PatchAnswer{
+        private Long answerId;
+        private Long userId;
+        private String body;
 
+        public void setAnswerId(Long answerId) {
+            this.answerId = answerId;
+        }
     }
 
-    public static class Response{
+    @Getter
+    public static class PatchVote{
+        private long userId;
+        private long answerId;
+        public void setAnswerId(long answerId){this.answerId = answerId;}
+    }
+    @Getter
+    public static class PatchAccept{
+        private long userId;
+        private long answerId;
+        public void setAnswerId(long answerId){this.answerId = answerId;}
+    }
 
+    @Getter
+    @Setter
+    @Builder
+    public static class Response{
+        private Long answerId;
+        private String body;
+        private Boolean accepted;
+        private int vote;
+        private Long userId;
+        private Long questionId;
     }
 }
