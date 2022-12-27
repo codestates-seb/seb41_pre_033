dede2837979d32768962b9627f779c5a34bfde7c
Rbaam <kimdg0814k@gmail.com>
2022-12-27 15:54:58 +0900

    fix: App1.js, App3.js 추가

---
+++ client/src/App3.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import Header from './header/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Ask from "./questions/pages/ask/Ask";
+import Navbar from "./components/navbar/Navbar";
+import Questions from "./questions/Questions";
+import Tags from "./tags/Tags";
+import Users from "./users/Users";
+import User from "./users/pages/User";
+import Login from "./users/pages/login/Login"
+import Signup from "./users/pages/login/Signup"
+
+function App() {
+  const tempDomain = "http://localhost:3001";
+  const [users, setUsers] = useState([]);
+  const [isLogin, setIsLogin] = useState(false);
+  const setLogin = () => setIsLogin(!isLogin);
+
+
+  useEffect(() => {
+    getUser();
+  }, [])
+  const getUser = () => {
+    return fetch(tempDomain+`/users`)
+    .then((res) => res.json())
+    .then((data) => {
+      setUsers(data)
+    })
+  };
+
+  return (
+    <div>
+    <Router>
+      <Header isLogin={isLogin} setLogin={setLogin}/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Questions />} />
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/tags" element={<Tags />} />
+        <Route path="/users" element={<Users domain={tempDomain}/>} />
+        <Route path="/users/:id/:user_nickname" element={<User users={users}/>}/>
+        <Route path="/users/login" element={<Login setLogin={setLogin} domain={tempDomain}/>}/>
+        <Route path="/users/sign-up" element={<Signup setLogin={setLogin} />}/>
+        <Route path="/dev1" /> {/* 코드 리뷰용 임시 루트1 */}
+        <Route path="/dev2" /> {/* 코드 리뷰용 임시 루트2 */}
+        <Route path="/dev3" /> {/* 코드 리뷰용 임시 루트3 */}
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
