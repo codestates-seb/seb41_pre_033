2ffa059bb1ae5d39ce9687504bb0bd8a606e57df
chaning49 <ggammancj@gmail.com>
2023-01-02 01:19:39 +0900

    feat: env.yml에 환경 변수 값 입력

--- server/src/main/java/server/ServerApplication.java
+++ server/src/main/java/server/ServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/env.yml")
 public class ServerApplication {
 
 	public static void main(String[] args) {
