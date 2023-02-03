87ca0a7bebad4a14c24379cbac405bcb91b96a5f
Rbaam <kimdg0814k@gmail.com>
2023-02-03 22:25:32 +0900

    refactor: 전반적인 수정 v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,26 @@
 import React, { useState } from 'react';
-import Header from './header/Header';
+import Header from './header/header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Questions from "./questions/Questions";
-import Users from "./users/Users";
-import Login from "./users/pages/login/Login";
-import Signup from "./users/pages/login/Signup";
-import User from "./users/pages/User";
-import EditUser from "./users/pages/EditUser";
-import AccountRecovery from "./users/pages/login/AccountRecovery";
-import Ask from "./questions/pages/ask/Ask";
-import Question from "./questions/pages/Question";
-import EditQuestion from "./questions/pages/edit/EditQuestion";
-import Tags from "./tags/Tags";
-
-
+import TopQuestions from './topQuestions';
+import Questions from './questions/questions';
+import Users from './users/users';
+import Tags from './tags/tags';
 
 function App() {
-
   const [isLogin, setIsLogin] = useState(false);
   const setLogin = () => setIsLogin(!isLogin);
 
   return (
     <div>
-    <Router>
-      <Header isLogin={isLogin} setLogin={setLogin}/>
-      <Routes>
-        {/* <Route path="/" element={<Questions />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/users/login" element={<Login setLogin={setLogin}/>}/>
-        <Route path="/users/sign-up" element={<Signup setLogin={setLogin} />}/>
-        <Route path="/users/:id/:user_nickname" element={<User />}/>
-        <Route path="/users/edit/:id" element={<EditUser />}/>
-        <Route path="/users/account-recovery" element={<AccountRecovery />}/>
-        <Route path="/questions" element={<Questions />} />
-        <Route path="/questions/ask" element={<Ask />} />
-        <Route path="/questions/:question_id" element={<Question />} />
-        <Route path="/questions/edit/:question_id" element={<EditQuestion />} />
-        <Route path="/tags" element={<Tags />} /> */}
-      </Routes>
-    </Router>
+      <Header isLogin={isLogin} setLogin={setLogin} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<TopQuestions />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/tags" element={<Tags />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- client/src/users/users.js
+++ client/src/users/users.js
@@ -1,54 +1,78 @@
 //다수의 유저 보여주는 페이지
-import './users.css'
-import { UserItem } from "./UserItem";
-import { Pagination } from "./pages/pagination";
+import './users.css';
+import { UserItem } from './userItem';
+import { Pagination } from './pages/pagination';
 import React, { useEffect, useState } from 'react';
-import { useQuery } from "react-query";
-import axios from "axios";
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
 const Users = () => {
-    const domain = "http://localhost:3001"
+  const domain = 'http://localhost:3001';
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
 
-//   const useUsers = (activePage) => {
-//     return useQuery(
-//       ["users", activePage],
-//       async () => {
-//         const { data } = await axios.get(
-//           `https://localhost:3001/users?page=${activePage}`
-//         );
-   
-//         return data;
-//       },
-//       { keepPreviousData: true }
-//     );
-//    };
-//    const usersList = useUsers(page);
-//    setUsers(usersList);
+  //   const useUsers = (activePage) => {
+  //     return useQuery(
+  //       ["users", activePage],
+  //       async () => {
+  //         const { data } = await axios.get(
+  //           `https://localhost:3001/users?page=${activePage}`
+  //         );
+
+  //         return data;
+  //       },
+  //       { keepPreviousData: true }
+  //     );
+  //    };
+  //    const usersList = useUsers(page);
+  //    setUsers(usersList);
 
   useEffect(() => {
     getUser();
-  }, [page])
+  }, [page]);
   const getUser = () => {
-    return fetch(domain+`/users?page=${page}`)
-    .then((res) => res.json())
-    .then((data) => {
-      setUsers(data)
-    })
+    return fetch(domain + `/users?page=${page}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
   };
-    return (
-        <div className='user-list-wrapper'>
-            <ul className='user-list-container'>
-                {users.map((e) => {
-                    return <UserItem key={e.userId} user={e}/>
-                })}
-            </ul>
-            <div>
-                <Pagination page={page} setPage={setPage} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="user-list-wrapper">
+      <ul className="user-list-container">
+        {users.map((e) => {
+          return <UserItem key={e.userId} user={e} />;
+        })}
+      </ul>
+      <div>
+        <Pagination page={page} setPage={setPage} />
+      </div>
+    </div>
+  );
+};
 
 export default Users;
+
+// 네스티드 라우팅 구현해주세요
+{
+  /*
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+  import Login from './users/pages/login/Login';
+import Signup from './users/pages/login/Signup';
+import User from './users/pages/user';
+import EditUser from './users/pages/EditUser';
+import AccountRecovery from './users/pages/login/AccountRecovery';
+
+
+<Router>
+  <Routes>
+    <Route path="/users/login" element={<Login setLogin={setLogin} />} />
+    <Route path="/users/sign-up" element={<Signup setLogin={setLogin} />} />
+    <Route path="/users/:id" element={<User />} />
+    <Route path="/users/edit/:id" element={<EditUser />} />
+    <Route path="/users/account-recovery" element={<AccountRecovery />} />
+  </Routes>
+</Router>;
+*/
+}
